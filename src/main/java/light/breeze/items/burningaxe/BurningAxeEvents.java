c9d35e827e5685c1af2daf6293a33c9043b4a43f
achoolucgust <lucgustga@outlook.com>
2024-03-02 23:14:53 +0800

    PANIC UPDATE because this part of the plugin crashes

--- src/main/java/light/breeze/items/burningaxe/BurningAxeEvents.java
+++ src/main/java/light/breeze/items/burningaxe/BurningAxeEvents.java
@@ -25,32 +25,33 @@ public class BurningAxeEvents implements Listener {
         m.put(0,"Fireball");
         m.put(1,"Machine Gun");
         m.put(2,"Enderdragon");
+        if (hand.hasItemMeta()&&hand.getItemMeta().hasCustomModelData()) {
+            int cmd = hand.getItemMeta().getCustomModelData();
+            int type = cmd-9003000;
+            if (event.getAction() == Action.RIGHT_CLICK_AIR||event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+                if (cmd > 9003000 && cmd < 9004000) {
+                    if (player.isSneaking()) {
+                        type = (type + 1) % 3;
+                        player.sendTitle("", "Ability set to: " + m.get(type), 5, 5, 5);
+                        ItemMeta meta = hand.getItemMeta();
+                        meta.setCustomModelData((type + 1) + 9003000);
+                        hand.setItemMeta(meta);
+                    } else if (!player.hasCooldown(hand.getType()) && player.getCooldown(hand.getType()) < 1) {
+                        Entity proj = null;
 
-        int cmd = hand.getItemMeta().getCustomModelData();
-        int type = cmd-9003000;
-        if (event.getAction() == Action.RIGHT_CLICK_AIR||event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            if (hand.hasItemMeta() && hand.getItemMeta().hasCustomModelData() && cmd > 9003000 && cmd < 9004000) {
-                if (player.isSneaking()) {
-                    type = (type + 1) % 3;
-                    player.sendTitle("", "Ability set to: " + m.get(type), 5, 5, 5);
-                    ItemMeta meta = hand.getItemMeta();
-                    meta.setCustomModelData((type + 1) + 9003000);
-                    hand.setItemMeta(meta);
-                } else if (!player.hasCooldown(hand.getType()) && player.getCooldown(hand.getType()) < 1) {
-                    Entity proj = null;
-
-                    if (type == 2) {
-                        proj = player.launchProjectile(SmallFireball.class);
-                        player.setCooldown(hand.getType(), 10);
-                    } else if (type == 3) {
-                        proj = player.launchProjectile(DragonFireball.class);
-                        player.setCooldown(hand.getType(), 120);
-                    } else {
-                        proj = player.launchProjectile(Fireball.class);
-                        player.setCooldown(hand.getType(), 60);
+                        if (type == 2) {
+                            proj = player.launchProjectile(SmallFireball.class);
+                            player.setCooldown(hand.getType(), 10);
+                        } else if (type == 3) {
+                            proj = player.launchProjectile(DragonFireball.class);
+                            player.setCooldown(hand.getType(), 120);
+                        } else {
+                            proj = player.launchProjectile(Fireball.class);
+                            player.setCooldown(hand.getType(), 60);
+                        }
+                        player.playSound(player.getLocation(), Sound.ITEM_FIRECHARGE_USE, SoundCategory.MASTER, 3, 0.75f);
+                        proj.setVelocity(player.getLocation().getDirection().multiply(4));
                     }
-                    player.playSound(player.getLocation(), Sound.ITEM_FIRECHARGE_USE, SoundCategory.MASTER, 3, 0.75f);
-                    proj.setVelocity(player.getLocation().getDirection().multiply(4));
                 }
             }
         }
