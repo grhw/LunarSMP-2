f224424191315f7a17cd2a2bc3ce61a4e53002d2
achoolucgust <lucgustga@outlook.com>
2024-03-21 20:54:05 +0800

    add pistons to the blacklist

--- src/main/java/light/breeze/anticheat/StopRedstoneClocks.java
+++ src/main/java/light/breeze/anticheat/StopRedstoneClocks.java
@@ -4,6 +4,7 @@ import light.breeze.utils.FileStorage;
 import light.breeze.utils.TickUtils;
 import light.breeze.utils.Utils;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -21,13 +22,16 @@ public class StopRedstoneClocks implements Listener {
 
     @EventHandler(priority = EventPriority.HIGH)
     public void stopRedstoneClocks(BlockRedstoneEvent event) {
-        if (event.getBlock().getType() == Material.REPEATER) {
+        if (event.getBlock().getType() == Material.REPEATER||event.getBlock().getType() == Material.PISTON) {
             String chunk = event.getBlock().getChunk().toString();
             String res = this.fs.get(chunk);
             Integer tick = tc.tc.redstoneTicksAlive;
             //Utils.log(tick.toString());
             //Utils.log(res);
             if (res != null&&Math.abs(tick-Integer.parseInt(res)) < 4) {
+                if (event.getNewCurrent() != 0) {
+                    event.getBlock().getWorld().playSound(event.getBlock().getLocation(), Sound.BLOCK_FIRE_EXTINGUISH,2,1);
+                }
                 event.setNewCurrent(0);
             } else {
                 this.fs.store(chunk, tick.toString());
